87756a60f686cba1e1daecc3818c239ab044c889
Morrison Cole <admin@morrisoncole.co.uk>
2019-07-28 17:43:57 +0900

    Migrate text to dice images

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +20,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText = findViewById<TextView>(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+        val diceImage = findViewById<ImageView>(R.id.dice_image)
+        val diceDrawable = when (randomInt) {
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(diceDrawable)
     }
 }
