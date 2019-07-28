07f98e54b442b37dcec2f33bfeefed5c93dc60a3
Morrison Cole <admin@morrisoncole.co.uk>
2019-07-29 07:34:28 +0900

    Completed app layout project

--- color-my-views/app/src/main/java/com/morrisoncole/colormyviews/MainActivity.kt
+++ color-my-views/app/src/main/java/com/morrisoncole/colormyviews/MainActivity.kt
@@ -21,7 +21,10 @@ class MainActivity : AppCompatActivity() {
             box_three_text,
             box_four_text,
             box_five_text,
-            constraint_layout)
+            constraint_layout,
+            red_button,
+            yellow_button,
+            green_button)
 
         for (clickableView in clickableViews) {
             clickableView.setOnClickListener { makeColored(it) }
@@ -39,6 +42,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
