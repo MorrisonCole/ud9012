c25dd168e7a0c27d5631c625a403885a165bd066
Morrison Cole <admin@morrisoncole.co.uk>
2019-07-29 14:25:25 +0900

    Use safe bundles

--- android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,9 +17,9 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
 
-        binding.playButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        )
+        binding.playButton.setOnClickListener {
+            it.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
 
         setHasOptionsMenu(true)
 
