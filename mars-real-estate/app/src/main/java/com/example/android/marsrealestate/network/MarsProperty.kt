ac316cdd1a483c34cb3245aa6748b93cbdbc6b38
Morrison Cole <admin@morrisoncole.co.uk>
2019-08-07 09:58:20 +0900

    Improved details formatting

--- mars-real-estate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ mars-real-estate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -28,4 +28,7 @@ data class MarsProperty(
         val imgSrcUrl: String,
         val type: String,
         val price: Double
-) : Parcelable
+) : Parcelable {
+    val isRental
+        get() = type == "rent"
+}
